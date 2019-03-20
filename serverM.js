07d4fba1f06044f7369ecfa930138ec2b30d7c7b
Bmargaux <43140549+Bmargaux@users.noreply.github.com>
2019-03-20 08:48:38 +0100

    serveur Margaux

---
+++ serverM.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+var first_try = require('./first_try')
+app.get('/', function(req, res) {
+
+    res.send('Hello World!');
+    
+});
+
+app.get('/startandstopchenillard', function(req, res) {
+    res.send('test');
+    //console.log("jesuisdansletest");
+    first_try.API_startandstop();
+});
+
+app.get('/speedchenillard', function(req, res) {
+    res.send('test');
+    first_try.API_changespeed();
+});
+
+app.get('/orderchenillard', function(req, res) {
+    res.send('test');
+    first_try.API_changeorder();
+});
+
+app.get('/disconnect', function(req, res) {
+    res.send('test');
+    first_try.API_disconnect();
+});
+
+
+
+
+
+
+server.listen(80, function(){
+    console.log('listening on *:80');
+});
