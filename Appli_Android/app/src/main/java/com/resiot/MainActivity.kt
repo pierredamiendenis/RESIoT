030d2e3ab4606a64fffa8ed5e180de816e2c2633
Bmargaux <43140549+Bmargaux@users.noreply.github.com>
2019-04-02 11:19:47 +0200

    okhttp

--- Appli_Android/app/src/main/java/com/resiot/MainActivity.kt
+++ Appli_Android/app/src/main/java/com/resiot/MainActivity.kt
@@ -3,6 +3,11 @@ package com.resiot
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import android.os.AsyncTask.execute
+import okhttp3.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +17,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        var btnStartStop = findViewById<Button>(R.id.button1)
+        btnStartStop.setOnClickListener(){
+
+            val client = OkHttpClient()
+
+            val formBody = FormBody.Builder()
+                .add("message", "Your message")
+                .build()
+            val request = Request.Builder()
+                .url("http://localhost:8080")
+                .post(formBody)
+                .build()
+
+            try {
+                val response = client.newCall(request).execute()
+
+                // Do something with the response.
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
+
         var btnConnect = findViewById<Button>(R.id.buttonConnect)
 
-        btnConnect.setOnClickListener(){
+        btnConnect.setOnClickListener{
             
         }
 
