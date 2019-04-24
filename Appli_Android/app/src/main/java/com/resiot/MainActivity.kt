74c4255b94d983b538efceed777eef8851e2771e
Bmargaux <43140549+Bmargaux@users.noreply.github.com>
2019-04-24 17:40:40 +0200

    front

--- Appli_Android/app/src/main/java/com/resiot/MainActivity.kt
+++ Appli_Android/app/src/main/java/com/resiot/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.resiot
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import org.json.JSONException
-import org.json.JSONObject
-import java.io.IOException
-import android.os.AsyncTask.execute
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import okhttp3.*
+import android.widget.Button
+import com.android.volley.toolbox.Volley
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
+
+    val url = "http://10.0.2.2:8080"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,12 +22,53 @@ class MainActivity : AppCompatActivity() {
 
 
         var btnStartStop = findViewById<Button>(R.id.button1)
-        btnStartStop.setOnClickListener(){
+
+        var jsonobj = JSONObject()
+
+        btnStartStop.setOnClickListener{
 
 
             Log.d("*****","Appuie sur bouton start")
 
-            val client = OkHttpClient()
+            jsonobj.put("name","coucou")
+
+            val que = Volley.newRequestQueue(this@MainActivity)
+            val req = JSONObjectRequest(Request.Method.GET,url,null
+            Response.Listener{
+                response ->
+                toast(response["success"].toString())
+
+            },Response.ErrorListener{
+                toast("Erreur")
+
+            })
+
+
+
+            //val requestQueue = Volley.newRequestQueue(MainActivity.this)
+           /*val requestQueue = Volley.newRequestQueue(this)
+           val stringReq = StringRequest(Request.Method.POST,url,
+
+               Response.Listener<String> { response ->
+
+                   // Ca marche
+                   requestQueue.stop()
+               },
+               Response.ErrorListener { textView!!.text = "That didn't work!"
+               requestQueue.stop()
+               })
+            queue.add(stringReq)*/
+
+
+
+
+            /*val okHttpClient = OkHttpClient()
+            val myGetRequest = Request.Builder()
+                .url("http://10.0.2.2:8080")
+                .build()*/
+
+
+            /*val client = OkHttpClient()
 
             val formBody = FormBody.Builder()
                 .add("message", "Your message")
@@ -43,16 +85,12 @@ class MainActivity : AppCompatActivity() {
                 // Do something with the response.
             } catch (e: IOException) {
                 e.printStackTrace()
-            }
+            }*/
 
         }
+
 
         var btnConnect = findViewById<Button>(R.id.buttonConnect)
-
-        btnConnect.setOnClickListener{
-            
-        }
-
 
 
     }
