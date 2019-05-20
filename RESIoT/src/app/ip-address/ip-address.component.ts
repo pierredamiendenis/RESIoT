36ba4723e097eec2d40c4115a12765ff7a6062f6
Bmargaux <43140549+Bmargaux@users.noreply.github.com>
2019-05-20 14:34:30 +0200

    ip

--- RESIoT/src/app/ip-address/ip-address.component.ts
+++ RESIoT/src/app/ip-address/ip-address.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators, FormGroup, FormBuilder} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-ip-address',
   templateUrl: './ip-address.component.html',
   styleUrls: ['./ip-address.component.scss']
 })
+
+
 export class IpAddressComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _formBuilder : FormBuilder) { }
+  
+ /* ipGroup: FormGroup;
 
   ngOnInit() {
+    this.ipGroup = this._formBuilder.group({
+      emailFormControl : ['',
+      Validators.compose([
+        Validators.required,
+        Validators.pattern('/^([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})[.]([0-9]{1,3})$/'),
+      ])
+    ]
+    })
+  }*/
+
+  knxGroup : FormGroup;
+
+  ngOnInit(){
+    this.knxGroup = this._formBuilder.group({
+      inputNameKnxControl: [
+        '',
+        Validators.compose([
+          Validators.required, 
+          Validators.minLength(2),
+          Validators.maxLength(254)
+        ])
+      ],
+      inputIpKnxControl: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(7),
+          Validators.maxLength(15),
+          Validators.pattern(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)
+        ])
+      ],
+      inputPortKnxControl: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(1),
+          Validators.pattern("^[0-9]*$"),
+          Validators.maxLength(5),
+        ]) 
+      ],
+
+    });
   }
+  
+
+ 
+
+  matcher = new MyErrorStateMatcher();
 
 }
